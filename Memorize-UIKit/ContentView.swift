02ea064de70ddf3c3608410771c371b0435cbe3c
Michael Griebling <sam@c-inspirations.com>
2021-07-28 06:11:46 -0400

    Initial Commit

---
+++ Memorize-UIKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize-UIKit
+//
+//  Created by Mike Griebling on 2021-07-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
